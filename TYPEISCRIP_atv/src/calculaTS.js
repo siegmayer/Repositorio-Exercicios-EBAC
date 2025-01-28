b7f944679f03c21af57f0a90298e4c9be623f80d
Brayan Franklin <160394545+siegmayer@users.noreply.github.com>
2025-01-28 19:27:57 -0300

    Add files via upload

---
+++ TYPEISCRIP_atv/src/calculaTS.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicaNumeros(num1, num2) {
+    return num1 * num2;
+}
+function saudacaoNome(nome) {
+    return "Olá " + nome;
+}
+const resultado = multiplicaNumeros(17, 18);
+const setNome = saudacaoNome("Brayan");
+console.log(resultado);
+console.log(setNome);
